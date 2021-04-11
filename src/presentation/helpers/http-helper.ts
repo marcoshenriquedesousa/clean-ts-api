047515c98e24676a58209540c6ba71f593f1fa0c
unknown <henriquesantos.uefa@gmail.com>
2021-04-11 10:06:47 -0300

    refactor: move success http response to a helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -10,3 +10,8 @@ export const serverError = (): HttpRespose => ({
   statusCode: 500,
   body: new ServerError()
 })
+
+export const ok = (data: any): HttpRespose => ({
+  statusCode: 200,
+  body: data
+})
